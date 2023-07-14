e5d966f84f71f3ccabff90dec572ea63939208a7
liconeto <benicio.neto9@gmail.com>
2023-07-14 17:52:49 -0300

    Api finalizada, començando front com NextJS

--- api/src/app.js
+++ api/src/app.js
@@ -1,8 +1,23 @@
+import cors from "cors";
 import express from "express";
+import fs from "fs";
+import https from "https";
+
 import router from "./routes.js";
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use(router);
 
-app.listen(3000, () => console.log("Api rodando!"));
+app.listen(3050, () => console.log("Api rodando!"));
+
+https
+  .createServer(
+    {
+      cert: fs.readFileSync("src/SSL/code.crt"),
+      key: fs.readFileSync("src/SSL/code.key"),
+    },
+    app
+  )
+  .listen(3051, () => console.log("Rodando em HTPPS!"));
